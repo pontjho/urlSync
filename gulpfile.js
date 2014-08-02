02a5547ed45c97ad033d5975682b524d821dc616
Pontjho Mokhonoana <pontjho@entelect.co.za>
2014-08-02 08:21:14 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var jsPath = ['urlSync.js'];
+var libPath = ['bower_components/angular/angular.js',
+               'lib/momentjs/moment.js'];
+
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src(jsPath)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(jsPath)
+        .pipe(rename('url-sync.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['lint']);
---
+++ urlSync.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('UrlSync', [])
+        .directive('urlSync', ['$location', '$filter', function ($location, $filter)
+    {
+        var definition = {
+            restrict: 'A',
+            scope: { ngModel: '=', name: '@', type: '@' },
+            link: function (scope, iElement, iAttrs)
+            {
+
+                var dateValue = $location.search()[scope.name];
+                var parsedValue = null;
+
+                if (scope.type == 'date') {
+                    var date = moment(dateValue);
+                    if(date.isValid())
+                    {
+                        parsedValue = new Date(parseInt(parts[0]), parseInt(parts[1]) - 1, parseInt(parts[2]));
+                    }
+                }
+                else {
+                    parsedValue = dateValue;
+                }
+
+                if(parsedValue)
+                {
+                    scope.ngModel = parsedValue;
+                }
+
+                scope.$watch('ngModel', function (newValue, oldValue) {
+
+                    if (newValue && oldValue) {
+                        if (scope.type == date && newValue instanceof Date) {
+                            var date = $filter('date')(newValue, 'yyyy-MM-dd');
+                            $location.search(scope.name, date);
+                        }
+                        else
+                            $location.search(scope.name, newValue);
+                    }
+
+                });
+            }
+        };
+        return definition;
+    }]);
+})();
